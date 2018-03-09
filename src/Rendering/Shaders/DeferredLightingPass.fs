047474c040976fe55384f00a9b7433d37291014b
Nin1 <jamesmhardy33@gmail.com>
2018-03-09 20:21:53 +0000

    directional lighting

--- src/Rendering/Shaders/DeferredLightingPass.fs
+++ src/Rendering/Shaders/DeferredLightingPass.fs
@@ -10,6 +10,14 @@ uniform sampler2D gAlbedoSpec;
 uniform sampler2D gEmissive;
 uniform sampler2D gShadowMap;
 
+uniform vec3 viewPos;
+uniform vec3 cameraViewMat;
+uniform mat4 shadowVPMat;
+uniform vec3 eyeSpaceLightPos;
+uniform vec3 directionalLightColour;
+uniform float directionalLightIntensity = 1.0;
+uniform vec3 directionalLightEyeDirection;
+
 struct PointLight
 {
 	vec3 pos;
@@ -20,10 +28,7 @@ struct PointLight
 const int MAX_POINT_LIGHTS = 32; // See DeferredLightingManager.h
 uniform PointLight pointLights[MAX_POINT_LIGHTS];
 
-uniform vec3 viewPos;
-uniform mat4 shadowVPMat;
-
-const float AMBIENT = 0.5;
+const float AMBIENT = 0.1;
 
 const vec2 POISSONDISK[4] = vec2[](
   vec2( -0.94201624, -0.39906216 ),
@@ -43,34 +48,51 @@ float GetLinearShadowMap(in vec2 uv)
 
 void main()
 {
-    // retrieve data from gbuffer
+	//** GET DATA FROM BUFFER */
+
     vec3 fragPos = texture(gPosition, texCoord).rgb;
-    vec3 normal = texture(gNormal, texCoord).rgb;
+    vec3 normal = normalize(texture(gNormal, texCoord).rgb);
     vec3 albedo = texture(gAlbedoSpec, texCoord).rgb;
     vec3 emissive = texture(gEmissive, texCoord).rgb;
     float specularAmount = texture(gAlbedoSpec, texCoord).a;
 
+	//** APPLY SHADOWS */
+
 	vec4 shadowSpacePos = shadowVPMat * vec4(fragPos, 1.0);
 	shadowSpacePos /= shadowSpacePos.w;
 	shadowSpacePos = shadowSpacePos * vec4(0.5) + vec4(0.5);
+	float dirLightAmount = 1.0;
 
 	for(int i = 0; i < 4; i++)
 	{
 		float shadowDepth = texture2D(gShadowMap, shadowSpacePos.xy + POISSONDISK[i]/700.0).x;
-		albedo = (shadowSpacePos.z > shadowDepth + 0.01) ? albedo - 0.1 : albedo;
+		dirLightAmount = (shadowSpacePos.z > shadowDepth + 0.01) ? dirLightAmount - 0.25 : dirLightAmount;
 	}
 
+    //** DIRECTIONAL LIGHTING */
     
-    // then calculate lighting as usual
-    vec3 lighting  = albedo * AMBIENT + emissive;
-    vec3 viewDir  = normalize(viewPos - fragPos);
+	vec3 lighting;
+	vec3 eyeSpaceFragPos = cameraViewMat * fragPos;
+	vec3 viewDir = normalize(viewPos - fragPos);
+	{
+		//vec3 lightDir = normalize(eyeSpaceLightPos - eyeSpaceFragPos);
+		vec3 lightDir = normalize(directionalLightEyeDirection);
+		vec3 diffuse = max(dot(normal, lightDir), 0) * albedo * directionalLightColour;
+		vec3 halfVec = normalize(normalize(eyeSpaceFragPos) + (-lightDir));
+		int facing = (dot(normal, lightDir) > 0) ? 1 : 0;
+		vec3 specular = vec3(1.0); //directionalLightColour * facing * directionalLightIntensity * pow(max(dot(normal, halfVec), 0), specularAmount);
+		lighting = vec3((AMBIENT * albedo) + (dirLightAmount * (emissive + diffuse * specular)));
+	}
+
+	//** POINT LIGHTS */
+
     for(int i = 0; i < MAX_POINT_LIGHTS; ++i)
     {
         // diffuse
         vec3 lightDir = normalize(pointLights[i].pos - fragPos);
         vec3 diffuse = max(dot(normal, lightDir), 0.0) * albedo * pointLights[i].colour;
         // specular
-        vec3 halfwayDir = normalize(lightDir + viewDir);  
+        vec3 halfwayDir = normalize((-lightDir) + normalize(eyeSpaceFragPos));  
         float spec = pow(max(dot(normal, halfwayDir), 0.0), 16.0);
         vec3 specular = pointLights[i].colour * spec * specularAmount;
         // attenuation
@@ -80,6 +102,9 @@ void main()
         specular *= attenuation;
         lighting += diffuse + specular;        
     }
+
+	//** OUTPUT COLOUR */
+
     fragColour = vec4(lighting, 1.0);
 	//fragColour = texture2D(gShadowMap, texCoord);
 }
